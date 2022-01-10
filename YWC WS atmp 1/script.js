3b5a7a6be3869ee95630e5353826be9e30878bdb
cinnamonica02 <96633571+cinnamonica02@users.noreply.github.com>
2022-01-10 13:44:53 -0500

    Add files via upload

---
+++ YWC WS atmp 1/script.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+window.addEventListener('scroll', function({
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+
+
+    function toggleMenu({
+        const menuToggle = document.querySelector('menuToggle');
+        menuToggle.classList.toggle('active');
+    })
+}));
+</script>
